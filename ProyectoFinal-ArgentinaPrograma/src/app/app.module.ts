24b5ddcd56c527191f04bb72e6ce57749a01fe5a
Carla Moreno <scarlamoreno@gmail.com>
2022-04-03 17:38:45 -0300

    Header

--- ProyectoFinal-ArgentinaPrograma/src/app/app.module.ts
+++ ProyectoFinal-ArgentinaPrograma/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    SocialmediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
